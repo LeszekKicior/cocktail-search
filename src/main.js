abcf5b3a47ef8f714f4fe06785062954bdb74b68
Leszek Kicior <leszek.kicior@fluentbe.com>
2020-09-02 01:03:41 +0200

    Vuebar added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuebar from 'vuebar';
+
+require('./assets/style.scss')
 
 Vue.config.productionTip = false
+Vue.use(Vuebar);
 
 new Vue({
   render: h => h(App),
