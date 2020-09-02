fff4b2b31f5c4a85d86c4b992d9c203f352e381c
Leszek Kicior <leszek.kicior@fluentbe.com>
2020-09-02 06:56:12 +0200

    Styles imported from new path

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuebar from 'vuebar';
 
-require('./assets/style.scss')
+require('./assets/styles/style.scss')
 
 Vue.config.productionTip = false
 Vue.use(Vuebar);
